4d41502264dfd295b734a41386ce85ca12d0726b
Filippo Mailli <filippo.mailli@gmail.com>
2020-05-28 01:21:21 +0200

    creazione relazioni

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Category;
+use App\InfoUser;
+use App\Page;
+use App\Tag;
+use App\Photo;
 
 class HomeController extends Controller
 {
@@ -23,6 +29,16 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $user = User::find(1);
+        $category = Category::find(1);
+        $page = Page::find(1);
+        // dd($user->info);
+        // dd($user->photos);
+        // dd($user->categories);
+        // dd($user->tags);
+        // dd($user->pages);
+        // dd($category->pages);
+        dd($page->photos);
         return view('home');
     }
 }
